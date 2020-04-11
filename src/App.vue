10965d76b917ab7256116306ae302de4458a1c05
LaFr4nc3 <lafr4nc3@gmail.com>
2020-04-12 01:44:31 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-content>
+      <v-container>
+        <Ammunition></Ammunition>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Ammunition from "@/components/Ammunition.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Ammunition
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  name = "App";
+}
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
